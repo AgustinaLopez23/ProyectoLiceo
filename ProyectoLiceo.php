3835f6193217df481d20b8f960a3fbc9b7882d57
Agustina lopez <al5261486@gmail.com>
2025-05-06 01:50:17 -0300

    Cambios realizados en el estilo general del sitio web(fondo degrade) y addicion de imagenes en los articulos

--- ProyectoLiceo.php
+++ ProyectoLiceo.php
@@ -9,9 +9,7 @@
     <meta name="keywords" content="fotografía, portfolio, Agustina Lopez, fotografía aficionada">
     <title>Portafolio de Agustina Lopez</title>
     <link rel="stylesheet" href="styles.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
